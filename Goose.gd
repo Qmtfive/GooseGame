30f0c39979202df287dc5a2a743844faa1bda1dd
Krivokrysenko <47541280+Krivokrysenko@users.noreply.github.com>
2021-11-06 20:53:17 -0400

    goose spawning no longer results in the apocalypse

--- Goose.gd
+++ Goose.gd
@@ -4,8 +4,10 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var speed = 300
-
+export var speed = 50
+export var xvel = 0
+export var yvel = 0
+var yveltempstore = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,10 +16,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2(5, 6)  # The player's movement vector.
+	var rand = RandomNumberGenerator.new()
+	rand.randomize()
+	xvel = rand.randf_range(-.75,.75)
+	var velocity = Vector2(xvel, yvel)
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$GooseAnimation.play()
 	else:
 		$GooseAnimation.stop()
 	position += velocity * delta
+
+func stop():
+	yveltempstore = yvel
+	yvel = 0
+	xvel = 0
+
+func changeDirection():
+	yvel = yveltempstore
--- GooseSpawner.gd
+++ GooseSpawner.gd
@@ -8,19 +8,24 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in range(0, 10):
+		var mothergoose = load("res://Goose.tscn")
+		var goose = mothergoose.instance()
+		var rand = RandomNumberGenerator.new()
+		rand.randomize()
+		goose.position.x = rand.randf_range(0, 475)
+		rand.randomize()
+		var boolean = rand.randi_range(0, 1)
+		if boolean == 0:
+			goose.position.y = -10
+			goose.yvel = 1
+		elif boolean == 1:
+			goose.position.y = 500
+			goose.yvel = -1
+		add_child(goose)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var rand = RandomNumberGenerator.new()
-	var mothergoose = load("res://Goose.tscn")
-	for i in range(0,10):
-		var goose = mothergoose.instance()
-		rand.randomize()
-		var x = rand.randf_range(0,100)
-		rand.randomize()
-		var y = rand.randf_range(0,100)
-		goose.position.y = y
-		goose.position.x = x
-		add_child(goose)
+	pass
+
