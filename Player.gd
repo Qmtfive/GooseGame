1b29cc63ec9a775104414c4286dfd6bb9eff2fae
Robert <rbamm@buffalo.edu>
2021-11-07 01:58:09 -0500

    Partially complete AttackCode

--- Player.gd
+++ Player.gd
@@ -3,6 +3,8 @@ extends Area2D
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 onready var anims = $PlayerAnims
+var attackdir = 0 # 0 to 3 in clockwise order (left up right, down)
+var attacking = false
 
 func _ready():
 	anims.play("WalkSide")
@@ -13,12 +15,16 @@ func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		attackdir = 0
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		attackdir = 2
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		attackdir = 3
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		attackdir = 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	if velocity.y > 0:
@@ -30,6 +36,7 @@ func _process(delta):
 		anims.flip_h = velocity.x > 0
 	else:
 		anims.stop()
+	
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
