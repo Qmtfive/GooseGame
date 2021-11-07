1914b79dafdc248a4ac0d05b655ab6427b7adc2f
Krivokrysenko <47541280+Krivokrysenko@users.noreply.github.com>
2021-11-07 01:47:26 -0400

    new animations implemented, they do be slidin' tho

--- Player.gd
+++ Player.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
+onready var anims = $PlayerAnims
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -10,19 +11,23 @@ func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		anims.play("WalkSide")
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		anims.play("WalkSide")
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		anims.play("WalkDown")
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		anims.play("WalkUp")
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$WalkingDude.play()
+		anims.play()
 	else:
-		$WalkingDude.stop()
+		anims.stop()
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	if velocity.x != 0:
-		$WalkingDude.flip_h = velocity.x > 0
+		anims.flip_h = velocity.x > 0
