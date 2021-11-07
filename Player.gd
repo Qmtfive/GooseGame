7d2acac0a3cb20cc120d7da6eb5e914e8cf20001
Krivokrysenko <47541280+Krivokrysenko@users.noreply.github.com>
2021-11-07 01:51:02 -0400

    they no longer slidin' but they do be moonwalkin'

--- Player.gd
+++ Player.gd
@@ -5,29 +5,31 @@ var screen_size  # Size of the game window.
 onready var anims = $PlayerAnims
 
 func _ready():
+	anims.play("WalkSide")
+	anims.stop()
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-		anims.play("WalkSide")
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-		anims.play("WalkSide")
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-		anims.play("WalkDown")
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-		anims.play("WalkUp")
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		anims.play()
+	if velocity.y > 0:
+		anims.play("WalkUp")
+	elif velocity.y < 0:
+		anims.play("WalkDown")
+	elif velocity.x != 0:
+		anims.play("WalkSide")
+		anims.flip_h = velocity.x > 0
 	else:
 		anims.stop()
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-	if velocity.x != 0:
-		anims.flip_h = velocity.x > 0
