3547d2a132c7c1990f78411986bb012f945e45bb
Krivokrysenko <47541280+Krivokrysenko@users.noreply.github.com>
2021-11-07 01:00:59 -0500

    stopping timer no longer stops the game entirely

--- Goose.gd
+++ Goose.gd
@@ -89,7 +89,8 @@ func _on_Goose_body_entered(body):
 func _on_Goose_area_entered(area):
 	if victim == null:
 		timeTillStop.stop()
-		timeTillWalkAgain.stop()
+		if timeTillWalkAgain != null:
+			timeTillWalkAgain.stop()
 		yveltempstore = yvel
 		if area.get_parent() != null:
 			victim = area.get_parent()
